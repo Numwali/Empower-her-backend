640b940729a79a8abf44bbf2b77f3d2332650a25
Numwali <n.umwali@alustudent.com>
2025-03-25 21:46:18 +0200

    appjs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
-/* eslint-disable */
 import app from "./index";
 import { ioConnect } from "./helpers/socketio";
 import httpServer from "http";
-
 const http = httpServer.Server(app);
 const port = process.env.PORT || 3000;
 
 try {
-  http.listen(port, () => {
-    console.log(`server running on port ${port} `);
+  http.listen(port, '0.0.0.0', () => {
+    console.log(`server running on port ${port}`);
   });
   ioConnect(http);
 } catch (error) {
   console.log(error);
 }
-
 export default http;
